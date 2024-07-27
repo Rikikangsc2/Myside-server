b2e319755e1c8b94683e161d9741485e990276a3
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-07-27 04:34:34 +0000

    H

--- index.js
+++ index.js
@@ -525,7 +525,7 @@ app.get('/gemini', async (req, res) => {
       return res.status(400).json({ error: 'Query parameter "q" is required' });
     }
 
-    const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBuEVDIfZkgdt90fZb2XHT66TNRZWgwumU', {
+    const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCtBDTdbx37uvBqiImuFdZFfAf5RD5igVY', {
       contents: [{
         parts: [{
           text: req.query.prompt
